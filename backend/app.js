2882ea3f6abf080f603cedc4937be514d5ed48fb
NiyumiNawalaella <niyumi2100@gmail.com>
2023-09-22 18:19:22 +0530

    connected to database

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
-mongoose.connect("mongodb+srv://admin:<password>@cluster0.8p3wseq.mongodb.net/?retryWrites=true&w=majority");
+//connecting to localhost Port which is 5000 and database
+mongoose.connect(
+    `mongodb+srv://admin:${process.env.MongoDB_Password}@cluster0.8p3wseq.mongodb.net/?retryWrites=true&w=majority`
+).then(() => app.listen(5000, () => 
+    console.log(`Connected To Database and Server`))
+). catch((e) => console.log(e));
+
+
 // app.use("/",(req,res,next) => {
 //     res.send("Hi");
 // });
 
-//connecting to localhost Port which is 5000
-app.listen(5000, () => {
-    console.log(`Connected To Localhost Port ${5000}`);
-});
 
-//MiJcx8pJj8N3fcvU
+
+//admin mongodb passwrod: MiJcx8pJj8N3fcvU
