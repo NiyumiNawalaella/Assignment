0a0ac4606b58c32ca0a9e1b9d8410347704c71fc
NiyumiNawalaella <niyumi2100@gmail.com>
2023-09-22 17:35:21 +0530

    conecting to localhost

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res,next) => {
+    res.send("Hi");
+});
+
+//connecting to localhost Port which is 5000
+app.listen(5000, () => {
+    console.log(`Connected To Localhost Port ${5000}`);
+});
