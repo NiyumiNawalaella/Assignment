23a53cd5e133d72bed7d72959644343e3dff754e
NiyumiNawalaella <niyumi2100@gmail.com>
2023-09-22 17:45:02 +0530

    install nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
-app.use("/",(req,res,next) => {
-    res.send("Hi");
-});
+// app.use("/",(req,res,next) => {
+//     res.send("Hi");
+// });
 
 //connecting to localhost Port which is 5000
 app.listen(5000, () => {
