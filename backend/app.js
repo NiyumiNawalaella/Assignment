2fdcfd77772a37bb10f295caef445330946b5602
NiyumiNawalaella <niyumi2100@gmail.com>
2023-09-22 18:07:15 +0530

    install mongoose for mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
+mongoose.connect("mongodb+srv://admin:<password>@cluster0.8p3wseq.mongodb.net/?retryWrites=true&w=majority");
 // app.use("/",(req,res,next) => {
 //     res.send("Hi");
 // });
@@ -10,3 +12,5 @@ const app = express();
 app.listen(5000, () => {
     console.log(`Connected To Localhost Port ${5000}`);
 });
+
+//MiJcx8pJj8N3fcvU
