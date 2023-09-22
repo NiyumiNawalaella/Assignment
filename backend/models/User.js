f2d15781b4ebe0a112069f9df1d21fe4f7ccb0b8
NiyumiNawalaella <niyumi2100@gmail.com>
2023-09-22 18:44:37 +0530

    saving data of users to mongodb

---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+import mongoose, { Mongoose } from "mongoose";
+const Schema =  mongoose.Schema;
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+});
+
+//we are exporting the mongose for every file to mongodb which will create collection of users.
+export default mongoose.model("User",userSchema)
